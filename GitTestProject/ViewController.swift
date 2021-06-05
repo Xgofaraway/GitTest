d553a7893003d1c23c1497bced3b43850e9ad345
xgo <xgofaraway>
2021-06-05 18:36:23 +0300

    Initial Commit

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by Efe Ağır on 5.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
